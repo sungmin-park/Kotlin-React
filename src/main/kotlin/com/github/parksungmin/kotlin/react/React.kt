e3520e2fa1e91ba54291e20bfb0ad12ca8541fff
vamf12 <vamf12@gmail.com>
2015-09-19 06:10:40 +0900

    add initial setup for kotlin2js

---
+++ src/main/kotlin/com/github/parksungmin/kotlin/react/React.kt
@@ -0,0 +1,11 @@
+package com.github.parksungmin.kotlin.react
+
+class React {
+    companion object {
+        val version = "0.0.1"
+
+        init {
+            println("Kotlin-React $version")
+        }
+    }
+}
