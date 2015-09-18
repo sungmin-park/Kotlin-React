adb7c6b03dd53f165609a624134c64fa883402db
vamf12 <vamf12@gmail.com>
2015-09-19 06:27:52 +0900

    ignore compiled files.

---
+++ src/test/kotlin/com/github/parksungmin/kotlin/react/ReactTest.kt
@@ -0,0 +1,11 @@
+package com.github.parksungmin.kotlin.react
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ReactTest {
+    @Test
+    fun test() {
+        assertEquals(2, 1 +1)
+    }
+}
